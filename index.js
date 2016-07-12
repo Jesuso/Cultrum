a468685e762fbf4e2c5d122dad9c008fa6d27178
Jesuso <jesuso.1410@gmail.com>
2016-07-11 22:57:54 -0500

    Changed directory structure, replaced the old http-server with express and socket.io.

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var express = require('express');
+var app = module.exports.app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(80);  //listen on port 80
+
+app.use(express.static('public'));
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
